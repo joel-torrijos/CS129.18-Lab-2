246f516c5925e7dffe08ce58131fb3b6ad2028f0
Jose Emmanuel Torrijos <jose.torrijos@obf.ateneo.edu>
2016-03-17 17:37:07 +0800

    Added new feature
    - Every Region of Interest (ROI) from a picture is created into an image
    file
    - Segregated the images into non-objects and objects

--- lab2.py
+++ lab2.py
@@ -15,17 +15,31 @@ def auto_canny(image, sigma=0.33):
 	# return the edged image
 	return edged
 
-og = cv2.imread('test.jpg',cv2.IMREAD_UNCHANGED)
-img = cv2.imread('test.JPG',0)
-edges = auto_canny(img)
+og = cv2.imread('test.jpg',cv2.IMREAD_UNCHANGED) # original picture
+ogWithContours = cv2.imread('test.jpg',cv2.IMREAD_UNCHANGED) # original picture with contours
+img = cv2.imread('test.JPG',0) # image for Canny
+edges = auto_canny(img) # 
 ret,thresh = cv2.threshold(edges,127,255,0)
 contours,hierarchy = cv2.findContours(thresh, 1, 2)
 
-for i in range(0, len(contours)-1):
+#For every contour, draw a rectangle surrounding it
+for i in range(0, len(contours)):
     cnt = contours[i]
     x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(og,(x,y),(x+w,y+h),(0,0,255))
+    r = cv2.boundingRect(cnt)
+    cv2.rectangle(ogWithContours,(x,y),(x+w,y+h),(0,0,255))
     #M = cv2.moments(cnt)
+    
+    # 3 to 4
+    # create image file for each ROI
+    fileName = "ROI-"
+    fileName += str(i)
+    fileName += ".jpg"    
+    cv2.imwrite(fileName, og[r[1]:r[1]+r[3], r[0]:r[0]+r[2]])
+    
 
-cv2.imshow("Original",og)
+cv2.imshow("Original",ogWithContours)
 cv2.imshow("Edges",edges)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
